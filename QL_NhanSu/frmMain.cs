21c1ef84d0d98a100e3ade49cfbd773a727ce8c4
nguyentronger98 <65801851+nguyentronger98@users.noreply.github.com>
2020-07-23 09:17:17 +0700

    Update

---
+++ QL_NhanSu/frmMain.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QL_NhanSu
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonItemMenuThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result;
+            result = MessageBox.Show("Bạn có muốn thoát khỏi hệ thống không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+            if (result == DialogResult.No)
+               e.Cancel = true;
+        }
+
+
+  
+        private void colorPickerDropDown1_SelectedColorChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+       
+
+
+      
+    }
+}
